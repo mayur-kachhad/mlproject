2a82f41ba1719b6b8898edf5b1ddc14f877d3d2f
mayur kachhad <mayurkachhad022@gmail.com>
2025-06-19 22:54:22 +0530

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -76,7 +76,7 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformer_object()
 
             target_column_name = "math_score"
-            numerical_columns = ["writing_score","reding_score"]
+            numerical_columns = ["writing_score","reeding_score"]
 
             input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df = train_df[target_column_name]
