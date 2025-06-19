f34e5d87d1fa3284a2d5b145bd4083687a927de7
mayur kachhad <mayurkachhad022@gmail.com>
2025-06-19 11:54:54 +0530

    Data injestion

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function return list of requirements
+    This function returns a list of requirements from requirements.txt
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
     return requirements
+
 setup(
     name="mlproject",
     version="0.0.1",
-    author='Mayur',
-    author_email='mayurkachhad022@gmail.com',
-    package=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    author="Mayur",
+    author_email="mayurkachhad022@gmail.com",
+    packages=find_packages(),  
+    install_requires=get_requirements("requirements.txt")
 )
